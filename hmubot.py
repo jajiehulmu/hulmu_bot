e17c7ff21bc32f48b956aceb52c91af9fbf2642c
jajiehulmu <42889970+jajiehulmu@users.noreply.github.com>
2018-09-01 21:33:54 +0800

    Update hmubot.py

--- hmubot.py
+++ hmubot.py
@@ -18,4 +18,10 @@ async def on_ready():
 async def ping(ctx):
     await bot.say(":ping_pong: ping!! x555")
 
+@bot.event
+async def on_member_join(member):
+    server = member.server
+    fmt = 'Welcome {0.mention} to {1.name}!'
+    await client.send_message(server, fmt.format(member, server))
+    
 bot.run("NDc4OTU3NTQ0OTU4NzIyMDUz.Dmvz8w.A9jeAQQnB14RaL97970A38V3j8s")
