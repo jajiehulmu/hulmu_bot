2c59c471b17eb9277a4ce140de7cdabc14522b5c
jajiehulmu <42889970+jajiehulmu@users.noreply.github.com>
2018-09-01 22:37:35 +0800

    Update hmubot.py

--- hmubot.py
+++ hmubot.py
@@ -10,12 +10,15 @@ bot = commands.Bot(command_prefix='#')
 @bot.event
 async def on_ready():
     
-    print ("Ready when you are Jajie")
-    print ("I am running on" + bot.user.name)
-    print ("with the ID: " + bot.user.id)
-
-@bot.command(pass_context=True)
-async def ping(ctx):
-    await bot.say(":ping_pong: hey")
+    print ("logged in as")
+    print (bot.user.name)
+    print (bot.user.id)
+    print ('-----')
     
+@bot.event
+async def on_member_join(member):
+    print("Recognized that a member called " + member.name + " joined")
+    await bot.send_message(member, "Welcome to the server!")
+    print('Sent message to " + member.name)
+          
 bot.run("NDc4OTU3NTQ0OTU4NzIyMDUz.Dmvz8w.A9jeAQQnB14RaL97970A38V3j8s")
