fa0e7c3d22931084f676d48f85c4f376c0563321
jajiehulmu <42889970+jajiehulmu@users.noreply.github.com>
2018-09-01 22:45:08 +0800

    Update hmubot.py

--- hmubot.py
+++ hmubot.py
@@ -6,6 +6,7 @@ from discord.ext.commands import Bot
 import asyncio
 
 bot = commands.Bot(command_prefix='#')
+client = discord.Client()
 
 @bot.event
 async def on_ready():
@@ -15,10 +16,10 @@ async def on_ready():
     print (bot.user.id)
     print ('-----')
     
-@bot.event
+@client.event
 async def on_member_join(member):
     print("Recognized that a member called " + member.name + " joined")
-    await bot.send_message(member, "Welcome to the server!")
+    await client.send_message(member, "Welcome to the server!")
     print("Sent message to " + member.name)
           
 bot.run("NDc4OTU3NTQ0OTU4NzIyMDUz.Dmvz8w.A9jeAQQnB14RaL97970A38V3j8s")
