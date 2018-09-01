07a7f6e4f2af41941f7330c998ffaac593a02a2c
jajiehulmu <42889970+jajiehulmu@users.noreply.github.com>
2018-09-01 20:29:23 +0800

    Add files via upload

---
+++ hmubot.py
@@ -0,0 +1,21 @@
+# hulmu bot By Erithreaj
+
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    
+    print ("Ready when you are Jajie")
+    print ("I am running on" + bot.user.name)
+    print ("with the ID: " + bot.user.id)
+
+@bot.command(pass_context=True)
+async def ping(ctx):
+    await bot.say(":ping_pong: ping!! x555")
+
+bot.run("NDc4OTU3NTQ0OTU4NzIyMDUz.Dmvz8w.A9jeAQQnB14RaL97970A38V3j8s")
